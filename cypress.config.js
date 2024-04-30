8a921241916793b0459998e34a30998563b2f8ed
Olha Kolodina <olha.kolodina@technorely.com>
2024-04-30 15:05:17 +0300

    lecture about finding elements be get, find and eq

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: "https://next.privat24.ua/",
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+
+  component: {
+    devServer: {
+      framework: "react",
+      bundler: "vite",
+    },
+  },
+});
