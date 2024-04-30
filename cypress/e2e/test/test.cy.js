ca63579f24e521a511386f1c536f7442cea5d068
Olha Kolodina <olha.kolodina@technorely.com>
2024-04-30 16:01:57 +0300

    add 2 lectures coode

--- cypress/e2e/test/test.cy.js
+++ cypress/e2e/test/test.cy.js
@@ -10,46 +10,46 @@
 //   });
 // });
 
-// it("By ID", () => {
-//   cy.visit("https://facebook.com/");
-//   cy.get("#email");
-// });
+it("By ID", () => {
+  cy.visit("https://facebook.com/");
+  cy.get("#email");
+});
 
-// it("By Class", () => {
-//   cy.visit(
-//     "https://docs.cypress.io/guides/core-concepts/introduction-to-cypress#Chains-of-Commands"
-//   );
-//   cy.get(".searchBox_H2mL");
-// });
+it("By Class", () => {
+  cy.visit(
+    "https://docs.cypress.io/guides/core-concepts/introduction-to-cypress#Chains-of-Commands"
+  );
+  cy.get(".searchBox_H2mL");
+});
 
-// it("By Tag", () => {
-//   cy.visit(
-//     "https://docs.cypress.io/guides/core-concepts/introduction-to-cypress#Chains-of-Commands"
-//   );
-//   cy.get("nav");
-// });
+it("By Tag", () => {
+  cy.visit(
+    "https://docs.cypress.io/guides/core-concepts/introduction-to-cypress#Chains-of-Commands"
+  );
+  cy.get("nav");
+});
 
-// it("By Tag value", () => {
-//   cy.visit("https://facebook.com/");
-//   cy.get('[name="pass"]');
-// });
+it("By Tag value", () => {
+  cy.visit("https://facebook.com/");
+  cy.get('[name="pass"]');
+});
 
-// it("By Diffent Tag", () => {
-//   cy.visit("https://facebook.com/");
-//   cy.get('[data-testid="open-registration-form-button"][role="button"]');
-// });
+it("By Diffent Tag", () => {
+  cy.visit("https://facebook.com/");
+  cy.get('[data-testid="open-registration-form-button"][role="button"]');
+});
 
-// it("By Diffent Types", () => {
-//   cy.visit(
-//     "https://docs.cypress.io/guides/continuous-integration/introduction"
-//   );
-//   cy.get('button[type="button"][aria-label="Close"]');
-// });
+it("By Diffent Types", () => {
+  cy.visit(
+    "https://docs.cypress.io/guides/continuous-integration/introduction"
+  );
+  cy.get('button[type="button"][aria-label="Close"]');
+});
 
-// it.only("By Contains name", () => {
-//   cy.visit("https://next.privat24.ua/");
-//   cy.get('*[class^="card"]');
-// });
+it.only("By Contains name", () => {
+  cy.visit("https://next.privat24.ua/");
+  cy.get('*[class^="card"]');
+});
 // Comment
 it("Using get with find an Eq", () => {
   cy.visit("https://next.privat24.ua/deposit/open");
