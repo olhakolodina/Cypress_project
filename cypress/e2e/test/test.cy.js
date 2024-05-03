6c6bb90fd5a216b4b7e5aaa367659b37be80ddeb
Olha Kolodina <olha.kolodina@technorely.com>
2024-05-03 10:50:19 +0300

    assertionsWithShouldAndExpect

--- cypress/e2e/test/test.cy.js
+++ cypress/e2e/test/test.cy.js
@@ -1,2 +1,47 @@
 /// <reference types="Cypress" />;
 
+it('SHOULD', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+    .clear()
+    .type(100)
+    .should('have.value', 100)
+    .and('be.visible')
+})
+
+it('EXPECT', ()=> {
+    cy.visit('https://next.privat24.ua/mobile?lang=en')
+    cy.get('[data-qa-node="amount"]')
+    .clear()
+    .type(100).then( input=> {
+        expect(input).to.have.value(100)
+    })
+    
+})
+
+it('check default value for Deposit', ()=> {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.get('[data-qa-value="UAH"]')
+    .should('be.checked')
+})
+
+it('check is visible archive link', ()=> {
+    cy.visit('https://next.privat24.ua/deposit/open?lang=en')
+    cy.contains('My deposits')
+    .trigger('mouseover')
+    .get('#archiveDeposits')
+    .should('be.visible')
+})
+
+it('check is correct attr in button', ()=> {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.contains('Show cards')
+    .should('have.attr', 'type')
+    .and('match', /button/)
+})
+
+it.only('check is correct URL', ()=> {
+    cy.visit('https://next.privat24.ua?lang=en')
+    cy.url()
+    .should('eq', 'https://next.privat24.ua/?lang=en')
+})
